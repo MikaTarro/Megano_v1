037e8c3afb6662999c683a37f78cbc8084e38049
MikaTarro <becoool2013@mail.ru>
2024-11-06 06:05:06 +0100

    spectac DRF -->

--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView, SpectacularRedocView
 
 
 urlpatterns = [
